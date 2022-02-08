c0428be7580729337fbd71c0521fb35e3b7cc33b
Alannegra <alanrodriguez@elpuig.xeill.net>
2022-02-08 19:01:09 +0100

    Actions4

--- src/test/java/ex2/HashTableTest.java
+++ src/test/java/ex2/HashTableTest.java
@@ -37,6 +37,22 @@ class HashTableTest {
                 " bucket[7] = [clave, valor]");
     }
 
+    void put2(){
+        HashTable hashTable = new HashTable();
+
+        hashTable.put("clave","valor");
+        hashTable.put("2","pera");
+        hashTable.put("3","manzana");
+        hashTable.put("3","naranja");
+        hashTable.put("5","platano");
+
+        Assertions.assertEquals(hashTable.toString(), "\n" +
+                " bucket[2] = [2, pera]\n" +
+                " bucket[3] = [3, manzana] -> [3, naranja]\n" +
+                " bucket[5] = [5, platano]\n" +
+                " bucket[7] = [clave, valor]");
+    }
+
 
     @org.junit.jupiter.api.Test
     void get() {
